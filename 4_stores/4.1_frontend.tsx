2c882f8189991790b64685dbd10518cbbc140402
Bernát Kalló <kallo.bernat@gmail.com>
2023-04-12 16:33:01 +0000

    Pending changes exported from your codespace

--- 4_stores/4.1_frontend.tsx
+++ 4_stores/4.1_frontend.tsx
@@ -2,9 +2,36 @@
 
 //
 
-function App() {
-  return <div>Hi</div>;
-}
 
 import { render } from "react-dom";
+import './4.4_volunteerStore';
+import { volunteerStore } from "./4.4_volunteerStore";
+import { observer } from "mobx-react-lite";
+
+const VolunteerList = observer(() => {
+  return <ul>
+    {volunteerStore.volunteers.map((volunteer) => <li>{volunteer.name}</li>)}
+  </ul>
+})
+
+
+const App = observer(() => {
+  return <div>
+    <VolunteerList/>
+    <form onSubmit={(e) => {
+      e.preventDefault()
+      volunteerStore.saveNewVolunteer()
+    }}>
+      New Volunteer:
+      <input placeholder="Name" 
+        value={volunteerStore.newVolunteerName}
+        onChange={(e) => volunteerStore.newVolunteerName = e.currentTarget.value}/>
+      <input placeholder="Phone"
+        value={volunteerStore.newVolunteerPhone}
+        onChange={(e) => volunteerStore.newVolunteerPhone = e.currentTarget.value}
+      />
+      <button type="submit">Save</button>
+    </form>
+  </div>;
+})
 render(<App />, document.getElementById("root"));
--- 4_stores/4.2_server.ts
+++ 4_stores/4.2_server.ts
@@ -1,1 +1,15 @@
 // Készíts el egy Remult + express szervert!
+
+import express from "express";
+import { remultExpress } from "remult/remult-express";
+import { Volunteer } from "./4.3_Volunteer";
+
+const app = express();
+app.use(
+  remultExpress({
+    entities: [Volunteer],
+  })
+);
+
+const port = 3002;
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
